6e34b20b916074f4b144c2d631a65d7cfd53cb2f
Petrus N Kirsten <petrus.kirsten@usp.br>
2025-08-14 19:28:20 -0300

    feat: fiz os schemas e models para os taxons

--- dinodex_api/especime/models.py
+++ dinodex_api/especime/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from sqlalchemy import DateTime, Integer, String, Float
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from dinodex_api.contrib.models import BaseModel
 
 
@@ -8,10 +8,13 @@ class EspecimeModel(BaseModel):
     __tablename__ = 'espécimes'
     
     pk_id         : Mapped[int]      = mapped_column(Integer,    primary_key=True)
-    nome          : Mapped[str]      = mapped_column(String(50), nullable=False)
+    apelido       : Mapped[str]      = mapped_column(String(50), nullable=False)
     codigo        : Mapped[str]      = mapped_column(String(11), nullable=False)
     idade         : Mapped[int]      = mapped_column(Integer,    nullable=False)
     peso          : Mapped[float]    = mapped_column(Float,      nullable=False)
     altura        : Mapped[float]    = mapped_column(Float,      nullable=False)
     conservacao   : Mapped[str]      = mapped_column(String(11), nullable=False)
     catalogado_em : Mapped[datetime] = mapped_column(DateTime,   nullable=False)
+
+    taxon    : Mapped['TaxonModel'] = relationship(back_populates='espécime')
+    taxon_id : Mapped[int] = mapped_column(ForeignKey('taxons.pk_id'))
---
+++ dinodex_api/taxons/schemas.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+from pydantic import Field
+
+from dinodex_api.contrib.schemas import BaseSchema
+
+
+class Taxon(BaseSchema):
+    nome: Annotated[
+        str, Field(description='Nome do taxon', examples='Theropoda', max_length=12)]
