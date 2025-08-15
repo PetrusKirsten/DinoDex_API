12214d560d087a15d9589562bc38993580fb5e6b
Petrus N Kirsten <petrus.kirsten@usp.br>
2025-08-15 18:36:47 -0300

    feat: fixes in env.py do alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,29 +1,23 @@
-from logging.config import fileConfig
+import asyncio
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from logging.config import fileConfig
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+from sqlalchemy import Connection, engine_from_config
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import async_engine_from_config
+
+from dinodex_api.contrib.models import BaseModel
+from dinodex_api.contrib.repository.models import *
+
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+target_metadata = BaseModel.metadata
 
 
 def run_migrations_offline() -> None:
@@ -38,6 +32,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
+    
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -50,6 +45,25 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(
+        connection=connection, target_metadata=target_metadata
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations():
+    connectable = async_engine_from_config(
+    config.get_section(config.config_ini_section, {}),
+    prefix="sqlalchemy.",
+    poolclass=pool.NullPool,
+    )
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
@@ -57,19 +71,8 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
+    
+    asyncio.run(run_async_migrations)
 
 
 if context.is_offline_mode():
---
+++ dinodex_api/contrib/repository/models.py
@@ -0,0 +1,4 @@
+from dinodex_api.especime.models import EspecimeModel
+from dinodex_api.taxons.models   import TaxonModel
+from dinodex_api.museu.models    import MuseuModel
+
